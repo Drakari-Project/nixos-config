c8ed9ab108b712ae131da703cfe3e74d2e679c84
DrakariDev <drakaridev@gmail.com>
2024-05-02 12:30:03 -0400

    removed extra kiosk user

--- configuration.nix
+++ configuration.nix
@@ -83,11 +83,6 @@
     packages = with pkgs; [];
   };
 
-  users.users.kiosk-drakari = {
-    isNormalUser = true;
-    description = "Drakari Kiosk User";
-  };
-
   # Enable automatic login for the user.
   services.getty.autologinUser = "drakari";
 
